9f710f5b2c7a6a90136679562521af04718b403f
Eric McCorkle <eric@metricspace.net>
2017-02-14 13:08:53 -0500

    Move MODINFOMD_KEYBUF definition to sys/linker.h

--- sys/arm64/include/metadata.h
+++ sys/arm64/include/metadata.h
@@ -31,7 +31,6 @@
 
 #define	MODINFOMD_EFI_MAP	0x1001
 #define	MODINFOMD_DTBP		0x1002
-#define	MODINFOMD_KEYBUF	0x1003
 
 struct efi_map_header {
 	size_t		memory_size;
--- sys/sparc64/include/metadata.h
+++ sys/sparc64/include/metadata.h
@@ -36,7 +36,6 @@
 #define	MODINFOMD_ITLB_SLOTS	0x1005
 #define	MODINFOMD_DTLB		0x1006
 #define	MODINFOMD_ITLB		0x1007
-#define	MODINFOMD_KEYBUF	0x1008
 
 struct tlb_entry {
 	vm_offset_t te_pa;
